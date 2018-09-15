4cce591d9154755661eb0cea9d7ebbade9016cf4
raiabe <gunyaa1441@gmail.com>
2018-09-15 18:28:11 +0000

    kansei

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('tasks','TasksController');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'TasksController@index');
